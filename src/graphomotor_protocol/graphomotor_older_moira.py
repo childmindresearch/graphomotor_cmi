8054d8d7965745ee3fa7624e54f1da6ded4ee964
childmind-mobi <mobi@childmind.org>
2026-02-12 11:16:37 -0500

    updating OLDER MOIRA to be same as OLDER harlem

--- src/graphomotor_protocol/graphomotor_older_moira.py
+++ src/graphomotor_protocol/graphomotor_older_moira.py
@@ -24,12 +24,10 @@ pygame.init()
 start_screen = pygame.display.set_mode((2560, 1400), pygame.RESIZABLE) # 1600x1200 
 start_screen_width, start_screen_height = start_screen.get_size()
 
-
 # Set up LSL stream
 info = StreamInfo(name='experiment_stream', type='Markers', channel_count=1,
                   channel_format='int32', source_id='uniqueid12345')
 outlet = StreamOutlet(info)
-
 
 # FUNCTIONS ##################
 ##############################
@@ -53,6 +51,7 @@ def start_button(start_screen):
 def show_start_screen():
     """Display the start screen with a button to begin and send event marker."""
     start_screen.fill((0, 0, 0))
+    #start_screen.blit(background_image, (0,0))
     font = pygame.font.Font(None, 60)
     title_surface = font.render("Graphomotor Protocol", True, (255, 255, 255))
     title_rect = title_surface.get_rect(center=(start_screen_width // 2, start_screen_height // 4))
@@ -73,30 +72,30 @@ def show_start_screen():
 
 def draw_back_button(screen):
     """Draws a "Back" button at the bottom left corner of the given Pygame screen."""
-    btn_w, btn_h = 150, 60
-    margin = 30
+    btn_w, btn_h = 130, 60
+    margin = 35
     # Place at bottom left
     x = margin 
     y = screen_height - btn_h - margin
     back_rect = pygame.Rect(x, y, btn_w, btn_h)
-    # White button
-    pygame.draw.rect(screen, (255, 255, 255), back_rect)
+    # Black button
+    pygame.draw.rect(screen, (0, 0, 0), back_rect) # white button (255, 255, 255)
     font = pygame.font.Font(None, 48)
-    # Black text for button 
-    back_surf = font.render("Back", True, (0,0,0))
+    # White text for button 
+    back_surf = font.render("Back", True, (255,255,255)) # black text (0, 0, 0)
     screen.blit(back_surf, back_surf.get_rect(center=back_rect.center))
     return back_rect
 
 def draw_forward_button(screen):
     """Draws a "Next" button at the bottom right corner of the given Pygame screen."""
-    btn_w, btn_h = 150, 60
-    margin = 30
+    btn_w, btn_h = 130, 60
+    margin = 35
     x = screen_width - btn_w - margin
     y = screen_height - btn_h - margin
     forward_rect = pygame.Rect(x, y, btn_w, btn_h)
-    pygame.draw.rect(screen, (255, 255, 255), forward_rect)
+    pygame.draw.rect(screen, (0, 0, 0), forward_rect)
     font = pygame.font.Font(None, 48)
-    forward_surf = font.render("Next", True, (0,0,0))
+    forward_surf = font.render("Next", True, (255,255,255))
     screen.blit(forward_surf, forward_surf.get_rect(center=forward_rect.center))
     return forward_rect
 
@@ -130,7 +129,8 @@ def draw_end_experiment_button(screen):
 
 def show_text_screen(text_lines):
     """Show text on screen with 'Back' and 'Next' buttons."""
-    screen.fill((0, 0, 0))
+    # screen.fill((0, 0, 0))
+    screen.blit(background_image, (0,0))
     font = pygame.font.Font(None, 60)
     y_offset = (screen_height - len(text_lines) * font.get_linesize()) // 2
     for line in text_lines:
@@ -159,7 +159,8 @@ def show_text_screen(text_lines):
 
 def show_text_screen_videos(text_lines):
     """Show text on screen with 'Back' and 'Next' buttons."""
-    screen.fill((0, 0, 0))
+    # screen.fill((0, 0, 0))
+    screen.blit(background_image, (0,0))
     font = pygame.font.Font(None, 60)
     y_offset = (screen_height - len(text_lines) * font.get_linesize()) // 2
     for line in text_lines:
@@ -223,7 +224,8 @@ def show_text_no_buttons(text_lines, duration_ms, event_markers):
     """Show text on screen without buttons. Need to specify duration in milliseconds."""
     markers = event_markers
     outlet.push_sample([markers[0]])
-    screen.fill((0, 0, 0))
+    # screen.fill((0, 0, 0))
+    screen.blit(background_image, (0,0))
     font = pygame.font.Font(None, 60)
     y_offset = (screen_height - len(text_lines) * font.get_linesize()) // 2
     
@@ -266,7 +268,8 @@ def play_audio(audio_file, num_times_play, text_lines, event_markers):
     """Play an audio file a specified number of times and display text on screen."""
     markers = event_markers
     outlet.push_sample([markers[0]])
-    screen.fill((0, 0, 0))
+    # screen.fill((0, 0, 0))
+    screen.blit(background_image, (0,0))
     font = pygame.font.Font(None, 60)
     y_offset = (screen_height - len(text_lines) * font.get_linesize()) // 2
     
@@ -302,9 +305,9 @@ def play_audio(audio_file, num_times_play, text_lines, event_markers):
 
 def play_video(video_path):
     """Play a video file with a background image with ET air tags."""
-    background_image_path = r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\videos\video_graphomotor2.jpg"
-    background_image = pygame.image.load(background_image_path)
-    background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
+    # background_image_path = r"C:\Users\MoBI\Desktop\Custom_MoBI_Software\files for protocol\video_graphomotor2.jpg"
+    # background_image = pygame.image.load(background_image_path)
+    # background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
 
     # to play video:
     player = MediaPlayer(video_path)
@@ -386,7 +389,6 @@ def play_videos_in_random_order(video_paths, event_markers):
         show_text_screen_videos(text_lines)
 
 
-
 # SCREENS ####################
 ##############################
 
@@ -444,7 +446,7 @@ speaker_rate_training_instrct = [
     "to the speed the sounds are being spoken.",
     "","", "Press 'Next' when you are ready."
 ]
-speaker_rate_training = ["Please pay attention to the speed of sounds and remain silent."]
+speaker_rate_training = ["LISTEN"]
 whisper_ta_instrc = [
     "Now it is your turn!",
     "When I say 'Go', start whispering 'ta' at the SAME SPEED as the",
@@ -483,40 +485,45 @@ show_start_screen()
 screen = pygame.display.set_mode((2560, 1340), pygame.NOFRAME) # 1600x1200, 1920x1080, 1280x1024, 1920,1200, 2560, 1440
 screen_width, screen_height = screen.get_size()
 
+# Load background image globally
+background_image_path = r"C:\Users\MoBI\Desktop\Custom_MoBI_Software\files for protocol\video_graphomotor2.jpg"
+background_image = pygame.image.load(background_image_path)
+background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
+
 ### Experiment Start, Resting State 
 protocol_flow(experiment_start, resting_state_instrc, event_markers=[[2,3], [4,5]])
 
 ### Resting State 
 # Eyes Open 1
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\open_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\open_eyes.wav", 1, cross, event_markers=[6,7])
+show_text_no_buttons(cross, 30000, event_markers=[85,86])
 # Eyes Closed 1
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\close_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\close_eyes.wav", 1, cross, event_markers=[87,88])
+show_text_no_buttons(cross, 30000, event_markers=[89,90])
 # Eyes Open 2
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\open_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\open_eyes.wav", 1, cross, event_markers=[91,92])
+show_text_no_buttons(cross, 30000, event_markers=[93,94])
 # Eyes Closed 2
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\close_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\close_eyes.wav", 1, cross, event_markers=[95,96])
+show_text_no_buttons(cross, 30000, event_markers=[97,98])
 # Eyes Open 3
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\open_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\open_eyes.wav", 1, cross, event_markers=[99,100])
+show_text_no_buttons(cross, 30000, event_markers=[101,102])
 # Eyes Closed 3
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\close_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\close_eyes.wav", 1, cross, event_markers=[103,104])
+show_text_no_buttons(cross, 30000, event_markers=[105,106])
 # Eyes Open 4
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\open_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\open_eyes.wav", 1, cross, event_markers=[107,108])
+show_text_no_buttons(cross, 30000, event_markers=[109,110])
 # Eyes Closed 4
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\close_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\close_eyes.wav", 1, cross, event_markers=[111,112])
+show_text_no_buttons(cross, 30000, event_markers=[113,114])
 # Eyes Open 5
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\open_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\open_eyes.wav", 1, cross, event_markers=[115,116])
+show_text_no_buttons(cross, 30000, event_markers=[117,118])
 # Eyes Closed 5
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\resting_state\close_eyes.wav", 1, cross, event_markers=[6,7])
-show_text_no_buttons(cross, 30000, event_markers=[6,7])
+play_audio(r"C:\Users\MoBI\Documents\resting_state_task\close_eyes.wav", 1, cross, event_markers=[119,120])
+show_text_no_buttons(cross, 30000, event_markers=[121,122])
 
 ### MindLogger
 protocol_flow(mindlogger_start, rey_copy_instrc, rey_copy, alpha_instrc, alpha, sprial_dominat_instrc, 
@@ -531,29 +538,32 @@ protocol_flow(mindlogger_start, rey_copy_instrc, rey_copy, alpha_instrc, alpha, 
 protocol_flow(break_screen, event_markers=[[38, 39]])
 
 ### Sync Audio Test
-protocol_flow(sync_audio_instrc, event_markers=[[42,43]])
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\sync_test\volume.wav", 4, increase_vol, event_markers=[44,45])
-protocol_flow(sync_test_instruc_1, speaker_rate_training_instrct, event_markers=[[46,47], [48,49]])
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\sync_test\example.wav", 1, speaker_rate_training, event_markers=[50,51])
-protocol_flow(whisper_ta_instrc, event_markers=[[52,53]])
-show_text_no_buttons(whisper_ta, 10000, event_markers=[54,55])
-protocol_flow(sync_test_instruc_2, event_markers=[[56,57]])
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\sync_test\stimulus.wav", 1, cross, event_markers=[58,59])
+protocol_flow(sync_audio_instrc, event_markers=[[40,41]])
+play_audio(r"C:\Users\MoBI\Documents\speech_sync_task\volume.wav", 8, increase_vol, event_markers=[42,43])
+protocol_flow(sync_test_instruc_1, speaker_rate_training_instrct, event_markers=[[44,45], [46,47]])
+play_audio(r"C:\Users\MoBI\Documents\speech_sync_task\example.wav", 1, speaker_rate_training, event_markers=[48,49])
+protocol_flow(whisper_ta_instrc, event_markers=[[50,51]])
+show_text_no_buttons(whisper_ta, 10000, event_markers=[52,53])
+protocol_flow(sync_test_instruc_2, event_markers=[[54,55]])
+play_audio(r"C:\Users\MoBI\Documents\speech_sync_task\stimulus.wav", 1, cross, event_markers=[56,57]) 
 # Run through a 2nd time
-protocol_flow(sync_test_instruc_1, speaker_rate_training_instrct, event_markers=[[60,61], [62,63]])
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\sync_test\example.wav", 1, speaker_rate_training, event_markers=[64,65])
-protocol_flow(whisper_ta_instrc, event_markers=[[66,67]])
-show_text_no_buttons(whisper_ta, 5000, event_markers=[68,69])
-protocol_flow(sync_test_instruc_2, event_markers=[[70,71]])
-play_audio(r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\sync_test\stimulus.wav", 1, cross, event_markers=[72,73])
+protocol_flow(sync_test_instruc_1, speaker_rate_training_instrct, event_markers=[[58,59], [60,61]])
+play_audio(r"C:\Users\MoBI\Documents\speech_sync_task\example.wav", 1, speaker_rate_training, event_markers=[62,63])
+protocol_flow(whisper_ta_instrc, event_markers=[[64,65]])
+show_text_no_buttons(whisper_ta, 5000, event_markers=[66,67])
+protocol_flow(sync_test_instruc_2, event_markers=[[68,69]])
+play_audio(r"C:\Users\MoBI\Documents\speech_sync_task\stimulus.wav", 1, cross, event_markers=[70,71])
+
+### Break 
+protocol_flow(break_screen, event_markers=[[72, 73]])
 
 ### Videos 
 protocol_flow(video_start_instrc, event_markers=[[74,75]])
 video_files = [
-    r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\videos\Diary of a Wimpy Kid Trailer.mp4",
-    r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\videos\despicable_me_clip.mp4",
-    r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\videos\the_present.mp4",
-    r"C:\Users\MoBI\Documents\graphomotor_protocol_2025\videos\fun_with_fractuals_vol_adj.mp4"
+    r"C:\Users\MoBI\Desktop\Custom_MoBI_Software\files for protocol\Diary of a Wimpy Kid Trailer.mp4",
+    r"C:\Users\MoBI\Desktop\Custom_MoBI_Software\files for protocol\despicable_me_clip.mp4",
+    r"C:\Users\MoBI\Desktop\Custom_MoBI_Software\files for protocol\the_present.mp4",
+    r"C:\Users\MoBI\Desktop\Custom_MoBI_Software\files for protocol\fun_with_fractuals_vol_adj.mp4"
 ]
 video_event_markers = {
     video_files[0]: [76, 77],
